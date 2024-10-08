2c19d31ed0a88388c3239a64e051a8f8060b7472
HumanBot000 <tobias.sarosi@gmail.com>
2024-10-08 21:51:25 +0200

    added getChallenges helper function

---
+++ lib/classes/Challenge.dart
@@ -0,0 +1,11 @@
+class Challenge {
+  String documentID;
+  String description;
+  int difficulty;
+  bool isOutdoor;
+  bool photosAllowed;
+  bool videosAllowed;
+
+  Challenge(this.documentID, this.description, this.difficulty, this.isOutdoor,
+      this.photosAllowed, this.videosAllowed);
+}
--- lib/game/challengeChooser/getChallenges.dart
+++ lib/game/challengeChooser/getChallenges.dart
@@ -1,11 +1,23 @@
+import 'package:appwrite/appwrite.dart';
+import 'package:appwrite_hackathon_2024/classes/Challenge.dart';
 import '../../enums/appwrite.dart';
 
-Future<List<String>> getChallenges({bool isOutdoor = true}) async {
+Future<List<Challenge>> getChallenges({bool isOutdoor = true}) async {
+  List<Challenge> challenges = [];
   final response = await databases.listDocuments(
-    databaseId: appDatabase,
-    collectionId: roomChallengesCollection,
-    queries: [
-
-    ]
-  )
+      databaseId: appDatabase,
+      collectionId: roomChallengesCollection,
+      queries: [Query.equal('	is_outdoor', isOutdoor)]);
+  for (var challenge in response.documents) {
+    var _challengeData = challenge.data;
+    challenges.add(Challenge(
+      challenge.$id,
+      _challengeData['description_en'],
+      _challengeData['difficulty'],
+      _challengeData['is_outdoor'],
+      _challengeData['photo_allowed'],
+      _challengeData['video_allowed'],
+    ));
+  }
+  return challenges;
 }
